6dc2bd40c859acc6eecadbaf06f1c13ee1889259
kevanoovitch <kiwikev00@gmail.com>
2025-04-14 21:50:28 +0200

    feat: output as a panel

--- PageCounter/Handlers/outputHandler.cs
+++ PageCounter/Handlers/outputHandler.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using PageCounter.Data;
+using Spectre.Console;
 
 namespace PageCounter.Handlers
 {
@@ -8,10 +10,18 @@ namespace PageCounter.Handlers
 
         public void PrintResult()
         {
+            var sb = new StringBuilder();
+
             foreach (var kvp in _result.ResultPlan)
             {
-                Console.WriteLine($"{kvp.Key}: {kvp.Value} \n");
+                //Console.WriteLine($"{kvp.Key}: {kvp.Value} \n");
+                sb.AppendLine($"{kvp.Key:yyyy-MM-dd}: {kvp.Value} pages");
             }
+            var panel = new Panel(sb.ToString())
+                .Header("📚 Reading Plan")
+                .Border(BoxBorder.Rounded);
+
+            AnsiConsole.Write(panel);
         }
     }
 }
